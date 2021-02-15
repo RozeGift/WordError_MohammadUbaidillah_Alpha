0c155ef9e9e6d4c2bd4f126a38649a02009eba49
RozeGift <72850017+RozeGift@users.noreply.github.com>
2021-02-16 05:10:08 +0800

    update

---
+++ Assets/Scripts/ThirdPersonController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonController : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float horizontalinput = Input.GetAxis("Horizontal");
+        float verticalinput = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(horizontalinput, 0f, verticalinput) * speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+    }
+}
